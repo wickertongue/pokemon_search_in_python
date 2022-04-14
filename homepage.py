b615b8f64af5a87368db4538176392720deb6961
wickertongue <haydenwb@gmail.com>
2022-04-14 22:02:15 +0100

    test(homepage templates/*): we can capture the user search and return this on another page. WIll need to try and use this data in a fetch to the api, and then return that data in html

--- homepage.py
+++ homepage.py
@@ -1,7 +1,13 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 
 home = Blueprint('home', __name__)
 
-@home.route('/')
+@home.route('/', methods = ['GET', 'POST'])
 def return_home():
     return render_template('homepage.html')
+
+@home.route('/post', methods = ['POST'])
+def post():
+    form_data = request.form
+    searched_pokemon = form_data["searchedPokemon"]
+    return render_template('response.html', searched_pokemon = searched_pokemon)
