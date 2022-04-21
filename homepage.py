7466b2ead34ab3fc7ab942478bd3119137feac0c
wickertongue <haydenwb@gmail.com>
2022-04-21 22:41:15 +0100

    wip(various): gradually building out the ability to view pokemon details in homepage.

--- homepage.py
+++ homepage.py
@@ -1,13 +1,27 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, json
+import requests
 
 home = Blueprint('home', __name__)
 
 @home.route('/', methods = ['GET', 'POST'])
 def return_home():
-    return render_template('homepage.html')
+    if request.method == "POST":
+        searched_pokemon = request.form["searchedPokemon"]
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{searched_pokemon}")
+        data = response.json()
 
-@home.route('/post', methods = ['POST'])
-def post():
-    form_data = request.form
-    searched_pokemon = form_data["searchedPokemon"]
-    return render_template('response.html', searched_pokemon = searched_pokemon)
+        # pokemon_data = json.dumps(data)
+        return render_template('homepage.html', found_pokemon = data)
+        # return redirect(url_for('pokemon', searched_pokemon = searched_pokemon))
+    else: 
+        # do something
+        return render_template('homepage.html')
+
+
+# def fetch_pokemon(pokemon):
+
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+#     data = response.json()
+
+#     pokemon_data = json.dumps(data)
+#     return(data)
