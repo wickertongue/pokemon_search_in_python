e0b3d72d4a20076364cae8ef1a05a51428aa6f9a
wickertongue <haydenwb@gmail.com>
2022-05-06 00:39:07 +0100

    feature(homepage.py): improved search function that finds first pokemon when search term is incomplete, also improved requests.get to make shorter, added comments for clarity

--- homepage.py
+++ homepage.py
@@ -6,13 +6,23 @@ home = Blueprint('home', __name__)
 @home.route('/', methods = ['GET', 'POST'])
 def return_home():
     if request.method == "POST":
-        searched_pokemon = request.form["searchedPokemon"]
-        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{searched_pokemon}")
-        data = response.json()
+        search_term = request.form["searchedPokemon"]
 
-        # pokemon_data = json.dumps(data)
+        # get all pokemon, turn it into json, return only results array which contains pokemon data
+        all_pokemon = (requests.get("https://pokeapi.co/api/v2/pokemon/?limit=2000").json())["results"]
+
+        # TODO: Can the below be done better? Tidier? More modular?
+        # Is it slow given it pulls all this data when the user hits enter?
+        def get_pokemon_url(search_term):
+            for pokemon in all_pokemon:
+                if (pokemon['name'].find(search_term) >= 0):
+                    found_pokemon_url = pokemon['url']
+                    return found_pokemon_url
+
+        data = requests.get(get_pokemon_url(search_term)).json()
+
         return render_template('homepage.html', found_pokemon = data)
-        # return redirect(url_for('pokemon', searched_pokemon = searched_pokemon))
+        # return redirect(url_for('pokemon', search_term = search_term))
     else: 
         # do something
         return render_template('homepage.html')
